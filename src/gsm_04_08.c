fad5d0db83c9ba458c865cfc1a645b83e98ddf19
Daniel Willmann <daniel@totalueberwachung.de>
2008-12-29 16:04:14 +0000

    Regenerate a valid TPDU and send it at first registration

--- src/gsm_04_08.c
+++ src/gsm_04_08.c
@@ -197,6 +197,8 @@ int gsm0408_loc_upd_acc(struct gsm_lchan *lchan, u_int32_t tmsi)
 
 	gsm48_sendmsg(msg);
 
+	gsm0411_send_sms(lchan, 0);
+
 	/* free the channel afterwards */   
 	ret = rsl_chan_release(lchan);
 
