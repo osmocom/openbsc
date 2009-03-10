955049f1910c3723f6d1f49c6f3fa6f4816bd479
Harald Welte <laforge@gnumonks.org>
2009-03-10 12:16:51 +0000

    import VTY code from zebra/quagga (from my cardshell project)

---
+++ src/vty/cardshell.h
@@ -0,0 +1,5 @@
+#define QUAGGA_PROGNAME	"OpenBSC"
+#define QUAGGA_VERSION	"0.01"
+#define QUAGGA_COPYRIGHT "Harald Welte <laforge@gnumonks.org>"
+#define CONFIGFILE_MASK 022
+#define SYSCONFDIR "/usr/local/etc"
