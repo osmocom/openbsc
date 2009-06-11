b89550764aac8b1f4b97734cc5451931a34c249d
Harald Welte <laforge@gnumonks.org>
2009-06-12 02:41:10 +0800

    Increase LCHAN_RELEASE_TIMEOUT to 10 seconds
    If we release the LCHAN after the CM SERVICE ACK too early, we can no longer
    receive longer SMS messages... so let's increase it to 10 secs.

--- openbsc/include/openbsc/gsm_data.h
+++ openbsc/include/openbsc/gsm_data.h
@@ -67,7 +67,7 @@ typedef int gsm_cbfn(unsigned int hooknum,
  * Use the channel. As side effect the lchannel recycle timer
  * will be started.
  */
-#define LCHAN_RELEASE_TIMEOUT 4, 0
+#define LCHAN_RELEASE_TIMEOUT 10, 0
 #define use_lchan(lchan) \
 	do {	lchan->use_count++; \
 		DEBUGP(DCC, "lchan (bts=%d,trx=%d,ts=%d,ch=%d) increases usage to: %d\n", \
