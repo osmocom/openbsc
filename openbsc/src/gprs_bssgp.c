c8b42938f6bbd1ffa39c36f5249ff13aa8a06ecc
Harald Welte <laforge@gnumonks.org>
2009-06-25 20:45:48 +0200

    intial skeleton work for GPRS support (in conjunction with nanoBTS)

---
+++ openbsc/src/gprs_bssgp.c
@@ -0,0 +1,83 @@
+/* GPRS Layer 3 protocol implementation as per 3GPP TS 08.18 */
+
+/* (C) 2009 by Harald Welte <laforge@gnumonks.org>
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <openbsc/msgb.h>
+#include <openbsc/tlv.h>
+#include <openbsc/gprs_bssgp.h>
+
+/* BSSGP has its own weird TLV encoding scheme, where the length
+ * can be one or two octet... if the first octet bit 7 is zero,
+ * then there is a second octet */
+static int bssgp_tlv_parse(struct tlv_parsed *tp, u_int8_t *data, int len)
+{
+	u_int8_t *cur = data;
+
+	while (cur < data) {
+		u_int8_t tag, *val;
+		u_int16_t len;
+
+		tag = *cur++;
+		if (*cur & 0x80)
+			len = *cur++ & 0x7f;
+		else
+			len = ((*cur++ & 0x7f) << 8) | *cur++;
+		val = *cur++;
+
+		tp[tag].len = len;
+		tp[tag].val = val;
+	}
+	return 0;
+}
+
+static int bssgp_rx_ul_ud(struct msgb *msg, u_int16_t bvci)
+{
+	struct bssgp_ud_hdr *budh = msgb->l3h;
+	struct tlv_parsed tp;
+	int data_len = l3_len(msg) - sizeof(*budh);
+	int rc;
+
+	rc = bssgp_tlv_parse(&tp, budh->data, data_len);
+
+	/* PDU_LIFETIME and LLC_PDU are the only mandatory IE */
+	if (!TLVP_PRESENT(&tp, BSSGP_IE_PDU_LIFETIME) ||
+	    !TLVP_PRESENT(&tp, BSSGP_IE_LLC_PDU))
+		return -EIO;
+
+	
+}
+
+/* We expect msg->l3h to point to the BSSGP header */
+int gprs_bssgp_rcvmsg(msg, u_int16_t bvci)
+{
+	struct gprs_bssgp_hdr *bgph = msgb->l3h;
+	u_int8_t pdu_type = bgph->pdu_data;
+	int rc;
+
+	switch (pdu_type) {
+	case BSSGP_PDUT_DL_UNITDATA:
+		rc = bssgp_rx_dl_ud(msg, bvci);
+		break;
+	case BSSGP_PDUT_UL_UNITDATA:
+		rc = bssgp_rx_ul_ud(msg, bvci);
+		break;
+	}
+}
---
+++ openbsc/src/gprs_ns.c
@@ -0,0 +1,99 @@
+/* GPRS Networks Service (NS) messages on the Gb interface 
+ * 3GPP TS 08.16 version 8.0.1 Release 1999 / ETSI TS 101 299 V8.0.1 (2002-05) */
+
+/* (C) 2009 by Harald Welte <laforge@gnumonks.org>
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <openbsc/msgb.h>
+#include <openbsc/talloc.h>
+#include <openbsc/gprs_ns.h>
+
+#define NS_ALLOC_SIZE	1024
+
+/* a layer 1 entity transporting NS frames */
+struct gprs_ns_link {
+	union {
+		struct {
+			int fd;
+		} ip;
+	};
+};
+
+static int gprs_ns_tx_simple(struct gprs_ns_link *link, u_int8_t pdu_type)
+{
+	struct msgb *msg = msgb_alloc(NS_ALLOC_SIZE);
+	struct gprs_ns_hdr *nsh;
+
+	if (!msgb)
+		return -ENOMEM;
+
+	nsh = msgb_put(msg, sizeof(*nsh));
+
+	nsh->pdu_type = pdu_type;
+	
+	/* FIXME: actually transmit */
+}
+
+static int gprs_ns_rx_unitdata(struct msgb *msg)
+{
+	struct gprs_ns_hdr *nsh = msgb->l2h;
+	u_int16_t bvci;
+
+	bvci = nsh->data[1] << 8 | nsh->data[2];
+	msgb->l3h = &nsh->data[3];
+
+	/* call upper layer (BSSGP) */
+	return bssgp_rcvmsg(msg, bvc);
+}
+
+/* main entry point, here incoming NS frames enter */
+int gprs_ns_rcvmsg(struct msgb *msg)
+{
+	struct gprs_ns_hdr *nsh = msgb->l2h;
+	int rc = -EINVAL;
+
+	switch (nsh->pdu_type) {
+	case NS_PDUT_ALIVE:
+		/* remote end inquires whether we're still alive,
+		 * we need to respond with ALIVE_ACK */
+		rc = gprs_ns_tx_simple(link, NS_PDUT_ALIVE_ACK);
+		break;
+	case NS_PDUT_ALIVE_ACK:
+		/* simply ignore it for now */
+		break;
+	case NS_PDUT_UNITDATA:
+		rc = gprs_ns_rx_unitdata(msg);
+		/* actual user data */
+		break;
+	case NS_PDUT_RESET:
+	case NS_PDUT_RESET_ACK:
+	case NS_PDUT_BLOCK:
+	case NS_PDUT_BLOCK_ACK:
+	case NS_PDUT_UNBLOCK:
+	case NS_PDUT_UNBLOCK_ACK:
+	case NS_PDUT_STATUS:
+		rc = 0;
+		break;
+	default:
+		break;	
+	}
+	return rc;
+}
+
