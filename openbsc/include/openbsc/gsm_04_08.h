798418a068fcc95f8f5bcad5ba923ab065d7fa86
Harald Welte <laforge@gnumonks.org>
2009-11-29 22:56:14 +0100

    [handover] Implement handover control logic
    Code to implement handover control logic.  A yet-to-be-implemented
    handover algorithm will call bsc_handover_start(old_lchan, new_bts)
    to start the handover process.

--- openbsc/include/openbsc/gsm_04_08.h
+++ openbsc/include/openbsc/gsm_04_08.h
@@ -757,6 +757,8 @@ int gsm48_send_rr_ciph_mode(struct gsm_lchan *lchan, int want_imeisv);
 int gsm48_send_rr_app_info(struct gsm_lchan *lchan, u_int8_t apdu_id,
 			   u_int8_t apdu_len, const u_int8_t *apdu);
 int gsm48_send_rr_ass_cmd(struct gsm_lchan *lchan, u_int8_t power_class);
+int gsm48_send_ho_cmd(struct gsm_lchan *old_lchan,
+		      struct gsm_lchan *new_lchan, u_int8_t power_command);
 
 int bsc_upqueue(struct gsm_network *net);
 
