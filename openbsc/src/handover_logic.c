ade773f44193052ade3be70b794130dd6a39ee06
Harald Welte <laforge@netfilter.org>
2009-12-21 13:29:19 +0100

    handover: set old channel to INACTIVE state
    After receiving the HANDOVER COMPLETE on the new channel, we mark the
    old channel as INACTIVE and try to release it ASAP.

--- openbsc/src/handover_logic.c
+++ openbsc/src/handover_logic.c
@@ -212,13 +212,16 @@ static int ho_gsm48_ho_compl(struct gsm_lchan *new_lchan)
 	}
 
 	bsc_del_timer(&ho->T3103);
-	llist_del(&ho->list);
 
 	/* update lchan pointer of transaction */
 	trans_lchan_change(ho->old_lchan, new_lchan);
 
+	ho->old_lchan->state = LCHAN_S_INACTIVE;
+	lchan_auto_release(ho->old_lchan);
+
 	/* do something to re-route the actual speech frames ! */
 
+	llist_del(&ho->list);
 	talloc_free(ho);
 
 	return 0;
