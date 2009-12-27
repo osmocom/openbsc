f6f2ef9d24d93ec339e5bedd8089138842813966
Harald Welte <laforge@gnumonks.org>
2009-12-27 11:24:55 +0100

    move get_string_value() und get_value_string() into debug.c
    this makes bsc_mgcp and ipaccess-proxy compile again

--- openbsc/src/debug.c
+++ openbsc/src/debug.c
@@ -56,6 +56,32 @@ static struct debug_category default_categories[Debug_LastEntry] = {
     [DREF]	= { .enabled = 0, .loglevel = LOGL_NOTICE },
 };
 
+const char *get_value_string(const struct value_string *vs, u_int32_t val)
+{
+	int i;
+
+	for (i = 0;; i++) {
+		if (vs[i].value == 0 && vs[i].str == NULL)
+			break;
+		if (vs[i].value == val)
+			return vs[i].str;
+	}
+	return "unknown";
+}
+
+int get_string_value(const struct value_string *vs, const char *str)
+{
+	int i;
+
+	for (i = 0;; i++) {
+		if (vs[i].value == 0 && vs[i].str == NULL)
+			break;
+		if (!strcasecmp(vs[i].str, str))
+			return vs[i].value;
+	}
+	return -EINVAL;
+}
+
 struct debug_info {
 	const char *name;
 	const char *color;
--- openbsc/src/gsm_data.c
+++ openbsc/src/gsm_data.c
@@ -32,32 +32,6 @@
 
 void *tall_bsc_ctx;
 
-const char *get_value_string(const struct value_string *vs, u_int32_t val)
-{
-	int i;
-
-	for (i = 0;; i++) {
-		if (vs[i].value == 0 && vs[i].str == NULL)
-			break;
-		if (vs[i].value == val)
-			return vs[i].str;
-	}
-	return "unknown";
-}
-
-int get_string_value(const struct value_string *vs, const char *str)
-{
-	int i;
-
-	for (i = 0;; i++) {
-		if (vs[i].value == 0 && vs[i].str == NULL)
-			break;
-		if (!strcasecmp(vs[i].str, str))
-			return vs[i].value;
-	}
-	return -EINVAL;
-}
-
 void set_ts_e1link(struct gsm_bts_trx_ts *ts, u_int8_t e1_nr,
 		   u_int8_t e1_ts, u_int8_t e1_ts_ss)
 {
