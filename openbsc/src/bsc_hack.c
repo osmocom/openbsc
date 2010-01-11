604d851b89d2d62f024223d333c833ebebd42060
Daniel Willmann <daniel@totalueberwachung.de>
2010-01-11 13:50:44 +0100

    Register GSM_BTS_TYPE_UNKNOWN in bsc_hack.c
    The way the VTY configuration sytem works is that it first registers a
    BTS of type GSM_BTS_TYPE_UNKNOWN and then sets the type correctly (after
    encountering the type statement). This makes sure that registering a BTS
    of type UNKNOWN succeeds.

--- openbsc/src/bsc_hack.c
+++ openbsc/src/bsc_hack.c
@@ -178,6 +178,7 @@ static void db_sync_timer_cb(void *data)
 	bsc_schedule_timer(&db_sync_timer, DB_SYNC_INTERVAL);
 }
 
+extern int bts_model_unknown_init(void);
 extern int bts_model_bs11_init(void);
 extern int bts_model_nanobts_init(void);
 
@@ -194,6 +195,7 @@ int main(int argc, char **argv)
 	stderr_target = debug_target_create_stderr();
 	debug_add_target(stderr_target);
 
+	bts_model_unknown_init();
 	bts_model_bs11_init();
 	bts_model_nanobts_init();
 
