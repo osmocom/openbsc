08db6ca509f8e06dc6aa686a92cad8613ccbc31e
Holger Hans Peter Freyther <zecke@selfish.org>
2010-04-05 17:57:10 +0200

    [mgcp] Reduce this to a debug message...
    This message can get in the way during a re-establishment
    of the MSC connection while the MSCs gateway is still sending
    data to us.

--- openbsc/src/mgcp/mgcp_network.c
+++ openbsc/src/mgcp/mgcp_network.c
@@ -126,7 +126,7 @@ static int rtp_data_cb(struct bsc_fd *fd, unsigned int what)
 
 	/* do not forward aynthing... maybe there is a packet from the bts */
 	if (endp->ci == CI_UNUSED) {
-		LOGP(DMGCP, LOGL_ERROR, "Unknown message on endpoint: 0x%x\n", ENDPOINT_NUMBER(endp));
+		LOGP(DMGCP, LOGL_DEBUG, "Unknown message on endpoint: 0x%x\n", ENDPOINT_NUMBER(endp));
 		return -1;
 	}
 
