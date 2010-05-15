e21bdea501d3c077f2148588091790def5bd0163
Holger Hans Peter Freyther <zecke@selfish.org>
2010-05-16 02:06:11 +0800

    [nat] Use and print the connection type of a SCCP connection.

--- openbsc/src/nat/bsc_nat.c
+++ openbsc/src/nat/bsc_nat.c
@@ -611,6 +611,7 @@ static int forward_sccp_to_msc(struct bsc_connection *bsc, struct msgb *msg)
 			if (create_sccp_src_ref(bsc, msg, parsed) != 0)
 				goto exit2;
 			con = patch_sccp_src_ref_to_msc(msg, parsed, bsc);
+			con->con_type = con_type;
 			break;
 		case SCCP_MSG_TYPE_RLSD:
 		case SCCP_MSG_TYPE_CREF:
--- openbsc/src/nat/bsc_nat_utils.c
+++ openbsc/src/nat/bsc_nat_utils.c
@@ -390,3 +390,15 @@ void bsc_parse_reg(void *ctx, regex_t *reg, char **imsi, int argc, const char **
 	}
 }
 
+static const char *con_types [] = {
+	[NAT_CON_TYPE_NONE] = "n/a",
+	[NAT_CON_TYPE_LU] = "Location Update",
+	[NAT_CON_TYPE_CM_SERV_REQ] = "CM Serv Req",
+	[NAT_CON_TYPE_PAG_RESP] = "Paging Response",
+	[NAT_CON_TYPE_OTHER] = "Other",
+};
+
+const char *bsc_con_type_to_string(int type)
+{
+	return con_types[type];
+}
