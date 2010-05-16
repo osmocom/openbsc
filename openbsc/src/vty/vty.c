00fd8b0e03be1d4912519d6ea9e9f2436cb570b8
Harald Welte <laforge@gnumonks.org>
2010-05-16 17:08:27 +0200

    vty: use install_node_ve() instead of separate ENABLE and VIEW node installs

--- openbsc/src/vty/vty.c
+++ openbsc/src/vty/vty.c
@@ -1645,16 +1645,14 @@ void vty_init()
 	/* Install bgp top node. */
 	install_node(&vty_node, vty_config_write);
 
-	install_element(VIEW_NODE, &config_who_cmd);
-	install_element(VIEW_NODE, &show_history_cmd);
-	install_element(ENABLE_NODE, &config_who_cmd);
+	install_element_ve(&config_who_cmd);
+	install_element_ve(&show_history_cmd);
 	install_element(CONFIG_NODE, &line_vty_cmd);
 	install_element(CONFIG_NODE, &service_advanced_vty_cmd);
 	install_element(CONFIG_NODE, &no_service_advanced_vty_cmd);
 	install_element(CONFIG_NODE, &show_history_cmd);
 	install_element(ENABLE_NODE, &terminal_monitor_cmd);
 	install_element(ENABLE_NODE, &terminal_no_monitor_cmd);
-	install_element(ENABLE_NODE, &show_history_cmd);
 
 	install_default(VTY_NODE);
 	install_element(VTY_NODE, &vty_login_cmd);
