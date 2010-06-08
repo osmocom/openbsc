b9a4fcfdaf54d21a0dc91910e7bff481f3ef82c1
Holger Hans Peter Freyther <zecke@selfish.org>
2010-06-08 13:45:10 +0800

    GPRS: Have a simple map from BSSGP PDU to the name...
    Sadly the FieldInfo does not provide this mapping...

--- openbsc/contrib/gprs/gprs-bssgp-histogram.lua
+++ openbsc/contrib/gprs/gprs-bssgp-histogram.lua
@@ -3,6 +3,22 @@
 do
 	local ip_bucket = {}
 
+	local pdu_types = {}
+	pdu_types[ 6] = "PAGING"
+	pdu_types[11] = "SUSPEND"
+	pdu_types[12] = "SUSPEND-ACK"
+	pdu_types[32] = "BVC-BLOCK"
+	pdu_types[33] = "BVC-BLOCK-ACK"
+	pdu_types[34] = "BVC-RESET"
+	pdu_types[35] = "BVC-RESET-ACK"
+	pdu_types[36] = "UNBLOCK"
+	pdu_types[37] = "UNBLOCK-ACK"
+	pdu_types[38] = "FLOW-CONTROL-BVC"
+	pdu_types[39] = "FLOW-CONTROL-BVC-ACK"
+	pdu_types[40] = "FLOW-CONTROL-MS"
+	pdu_types[41] = "FLOW-CONTROL-MS-ACK"
+	pdu_types[44] = "LLC-DISCARDED"
+
 	local function init_listener()
 		-- handle the port as NS over IP
 		local udp_port_table = DissectorTable.get("udp.port")
@@ -43,7 +59,7 @@ do
 			end
 
 			table.insert(bucket, tostring(len))
-			print("IP: " .. ip_src .. " PDU: " .. pdu .. " Length: " .. tostring(len))
+			print("IP: " .. ip_src .. " PDU: " .. pdu_types[tonumber(pdu)] .. " Length: " .. tostring(len))
 		end
 
 		function tap.draw()
