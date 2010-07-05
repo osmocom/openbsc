4f448c97ebc8dae095e109965ec038601125a7c8
Holger Hans Peter Freyther <zecke@selfish.org>
2010-07-05 16:02:04 +0800

    bsc: Add header file for the true BSC API.

---
+++ openbsc/include/openbsc/osmo_bsc.h
@@ -0,0 +1,10 @@
+/* OpenBSC BSC code */
+
+#ifndef OSMO_BSC_H
+#define OSMO_BSC_H
+
+#include "bsc_api.h"
+
+struct bsc_api *osmo_bsc_api();
+
+#endif
---
+++ openbsc/src/bsc/osmo_bsc_api.c
@@ -0,0 +1,26 @@
+/* (C) 2009-2010 by Holger Hans Peter Freyther <zecke@selfish.org>
+ * (C) 2009-2010 by On-Waves
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <openbsc/osmo_bsc.h>
+
+struct bsc_api *osmo_bsc_api()
+{
+	return NULL;
+}
