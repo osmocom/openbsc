80dfa75df2541a04cbb74960f4fa07410ad443ef
Holger Hans Peter Freyther <zecke@selfish.org>
2010-07-26 17:59:28 +0800

    gsm_04_80: Fix the style and move the '*' to the function

--- openbsc/include/openbsc/gsm_04_80.h
+++ openbsc/include/openbsc/gsm_04_80.h
@@ -23,7 +23,7 @@ int gsm0480_send_ussd_reject(struct gsm_subscriber_connection *conn,
 			     const struct msgb *msg, 
 			     const struct ussd_request *request);
 
-struct msgb * gsm0480_create_notifySS(const char *text);
+struct msgb *gsm0480_create_notifySS(const char *text);
 int gsm0480_wrap_invoke(struct msgb *msg, int op, int link_id);
 
 #endif
