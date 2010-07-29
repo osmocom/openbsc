0dcacda1943591be9785cd5ea9063519a600cb9f
Holger Hans Peter Freyther <zecke@selfish.org>
2010-07-29 19:01:43 +0800

    osmo_rf: Keep the current policy inside the RF struct..
    Keep a back pointer to the rf struct inside the connection,
    resolve the network through the back pointer. Also assume
    that the RF is on. In case we start with RF locked, the policy
    is on but we will not see any MS talking to us.

--- openbsc/include/openbsc/bsc_msc_rf.h
+++ openbsc/include/openbsc/bsc_msc_rf.h
@@ -6,13 +6,15 @@
 struct gsm_network;
 
 struct bsc_msc_rf {
+	/* the value of signal.h */
+	int policy;
 	struct bsc_fd listen;
 	struct gsm_network *gsm_network;
 };
 
 struct bsc_msc_rf_conn {
 	struct write_queue queue;
-	struct gsm_network *gsm_network;
+	struct bsc_msc_rf *rf;
 };
 
 struct bsc_msc_rf *bsc_msc_rf_create(const char *path, struct gsm_network *net);
