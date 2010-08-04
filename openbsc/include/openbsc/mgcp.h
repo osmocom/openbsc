bb89aa1430872598cec1f2508cefd040efaefc87
Holger Hans Peter Freyther <zecke@selfish.org>
2010-08-05 03:29:33 +0800

    mgcp: Make the function internal, only used by the init/config code

--- openbsc/include/openbsc/mgcp.h
+++ openbsc/include/openbsc/mgcp.h
@@ -111,7 +111,6 @@ struct mgcp_config *mgcp_config_alloc(void);
 int mgcp_parse_config(const char *config_file, struct mgcp_config *cfg);
 int mgcp_vty_init(void);
 int mgcp_endpoints_allocate(struct mgcp_config *cfg);
-int mgcp_bind_bts_rtp_port(struct mgcp_endpoint *endp, int rtp_port);
 void mgcp_free_endp(struct mgcp_endpoint *endp);
 
 /*
