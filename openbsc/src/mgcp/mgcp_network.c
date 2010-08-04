osmocom/openbsc

commit 7cdc62c01223570441e60ec6d0dd7a92e622677b

mgcp: Fix the reversed net/bts... which has not cause any issue..

openbsc/src/mgcp/mgcp_network.c
@@ -238,7 +238,7 @@ static int rtp_data_cb(struct bsc_fd *fd, unsigned int what)
 	if (dest == DEST_NETWORK) {
 		if (proto == PROTO_RTP)
 			patch_and_count(endp, &endp->bts_state,
-					endp->net_payload_type,
+					endp->bts_payload_type,
 					&addr, buf, rc);
 		return udp_send(fd->fd, &endp->remote,
 			     proto == PROTO_RTP ? endp->net_rtp : endp->net_rtcp,
@@ -246,7 +246,7 @@ static int rtp_data_cb(struct bsc_fd *fd, unsigned int what)
 	} else {
 		if (proto == PROTO_RTP)
 			patch_and_count(endp, &endp->net_state,
-					endp->bts_payload_type,
+					endp->net_payload_type,
 					&addr, buf, rc);
 		return udp_send(fd->fd, &endp->bts,
 			     proto == PROTO_RTP ? endp->bts_rtp : endp->bts_rtcp,

commit 7f8aec2b2b527f2c994349dd2eda76eb745a7658

mgcp: Fix the payload_type... it broke in 7cdc62c01223570441e60ec6d0dd7a92e622677b

openbsc/src/mgcp/mgcp_network.c
@@ -238,7 +238,7 @@ static int rtp_data_cb(struct bsc_fd *fd, unsigned int what)
 	if (dest == DEST_NETWORK) {
 		if (proto == PROTO_RTP)
 			patch_and_count(endp, &endp->bts_state,
-					endp->bts_payload_type,
+					endp->net_payload_type,
 					&addr, buf, rc);
 		return udp_send(fd->fd, &endp->remote,
 			     proto == PROTO_RTP ? endp->net_rtp : endp->net_rtcp,
@@ -246,7 +246,7 @@ static int rtp_data_cb(struct bsc_fd *fd, unsigned int what)
 	} else {
 		if (proto == PROTO_RTP)
 			patch_and_count(endp, &endp->net_state,
-					endp->net_payload_type,
+					endp->bts_payload_type,
 					&addr, buf, rc);
 		return udp_send(fd->fd, &endp->bts,
 			     proto == PROTO_RTP ? endp->bts_rtp : endp->bts_rtcp,