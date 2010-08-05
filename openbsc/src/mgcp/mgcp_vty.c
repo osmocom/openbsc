889fd79511fccf9325cfad0afeae8e9d97981717
Holger Hans Peter Freyther <zecke@selfish.org>
2010-08-05 08:08:17 +0800

    mgcp: Allow to dynamically allocate ports from a range..
    Allow to switch to a dynamic port allocator and not reuse
    the ports for a long time... This should help with a crazy
    network sending two streams at the same time.

--- openbsc/src/mgcp/mgcp_vty.c
+++ openbsc/src/mgcp/mgcp_vty.c
@@ -374,6 +374,7 @@ int mgcp_parse_config(const char *config_file, struct mgcp_config *cfg)
 				LOGP(DMGCP, LOGL_FATAL, "Failed to bind: %d\n", rtp_port);
 				return -1;
 			}
+			endp->bts_end.local_alloc = PORT_ALLOC_STATIC;
 		}
 
 		if (g_cfg->net_ports.mode == PORT_ALLOC_STATIC) {
@@ -383,6 +384,7 @@ int mgcp_parse_config(const char *config_file, struct mgcp_config *cfg)
 				LOGP(DMGCP, LOGL_FATAL, "Failed to bind: %d\n", rtp_port);
 				return -1;
 			}
+			endp->net_end.local_alloc = PORT_ALLOC_STATIC;
 		}
 	}
 
