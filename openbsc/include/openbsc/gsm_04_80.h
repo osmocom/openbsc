4cc21eaa42bd90928d0b5c1d3f52713a17abdff3
Holger Hans Peter Freyther <zecke@selfish.org>
2010-10-18 09:53:04 +0200

    ussd: Move the code libosmocore, increase the version number.
    Move the code to libosmocore, update the header file and the
    version required in the configure.in.
    
    Conflicts:
    
    	openbsc/configure.in
    	openbsc/include/openbsc/gsm_04_80.h
    	openbsc/src/gsm_04_80.c

--- openbsc/include/openbsc/gsm_04_80.h
+++ openbsc/include/openbsc/gsm_04_80.h
@@ -3,17 +3,8 @@
 
 #include <osmocore/msgb.h>
 #include <osmocore/protocol/gsm_04_80.h>
+#include <osmocore/gsm0480.h>
 
-#define MAX_LEN_USSD_STRING	31
-
-struct ussd_request {
-			char text[MAX_LEN_USSD_STRING + 1];
-			u_int8_t transaction_id;
-			u_int8_t invoke_id;
-};
-
-int gsm0480_decode_ussd_request(const struct msgb *msg, 
-				struct ussd_request *request); 
 int gsm0480_send_ussd_response(const struct msgb *in_msg, const char* response_text, 
 						const struct ussd_request *req);
 int gsm0480_send_ussd_reject(const struct msgb *msg, 
