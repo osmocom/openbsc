928ca743db057902337ca282e6a12d8a5389df4f
Holger Hans Peter Freyther <zecke@selfish.org>
2010-11-10 09:19:30 +0100

    subscr: Move the TMSI reserved into the gsm_data
    Put it into the gsm_data.h to avoid requiring MSC code
    in the BSC code paths.

--- openbsc/include/openbsc/gsm_subscriber.h
+++ openbsc/include/openbsc/gsm_subscriber.h
@@ -13,10 +13,6 @@
 #define GSM_MIN_EXTEN 20000
 #define GSM_MAX_EXTEN 49999
 
-/* reserved according to GSM 03.03 § 2.4 */
-#define GSM_RESERVED_TMSI   0xFFFFFFFF
-
-
 #define GSM_SUBSCRIBER_FIRST_CONTACT	0x00000001
 #define tmsi_from_string(str) strtoul(str, NULL, 10)
 
