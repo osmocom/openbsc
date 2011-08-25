baa1a2df2086b1c920053c1aaa6062929f2256e4
Holger Hans Peter Freyther <zecke@selfish.org>
2011-08-25 09:15:40 +0200

    bsc: Do not re-start the grace timer when we are in the grace period

--- openbsc/src/libbsc/bsc_rf_ctrl.c
+++ openbsc/src/libbsc/bsc_rf_ctrl.c
@@ -239,6 +239,11 @@ static void grace_timeout(void *_data)
 
 static int enter_grace(struct osmo_bsc_rf *rf)
 {
+	if (osmo_timer_pending(&rf->grace_timeout)) {
+		LOGP(DLINP, LOGL_NOTICE, "RF Grace timer is pending. Not restarting.\n");
+		return 0;
+	}
+
 	rf->grace_timeout.cb = grace_timeout;
 	rf->grace_timeout.data = rf;
 	osmo_timer_schedule(&rf->grace_timeout, rf->gsm_network->bsc_data->mid_call_timeout, 0);
