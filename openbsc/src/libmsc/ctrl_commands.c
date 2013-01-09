db695fc0f68ab816946b51a6e8280e3bef54b9f1
Holger Hans Peter Freyther <zecke@selfish.org>
2013-01-09 19:52:14 +0100

    nitb: Introduce a command to set the description of a BTS

--- openbsc/src/libmsc/ctrl_commands.c
+++ openbsc/src/libmsc/ctrl_commands.c
@@ -30,6 +30,7 @@ CTRL_CMD_DEFINE_RANGE(bts_mnc, "mnc", struct gsm_bts, network_code, 0, 999);
 CTRL_CMD_DEFINE_RANGE(bts_mcc, "mcc", struct gsm_bts, country_code, 1, 999);
 CTRL_CMD_DEFINE_STRING(bts_short_name, "short-name", struct gsm_bts, name_short);
 CTRL_CMD_DEFINE_STRING(bts_long_name, "long-name", struct gsm_bts, name_long);
+CTRL_CMD_DEFINE_STRING(bts_description, "description", struct gsm_bts, description);
 
 static int verify_net_save_config(struct ctrl_cmd *cmd, const char *v, void *d)
 {
@@ -92,6 +93,7 @@ int bsc_ctrl_cmds_install(void)
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_mcc);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_short_name);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_long_name);
+	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_description);
 
 	rc |= ctrl_cmd_install(CTRL_NODE_ROOT, &cmd_net_apply_config);
 	rc |= ctrl_cmd_install(CTRL_NODE_ROOT, &cmd_net_save_config);
