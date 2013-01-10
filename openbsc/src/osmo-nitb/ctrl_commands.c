e9d7c9c53211df2391b7ff326b5d80cd03c4a07d
Holger Hans Peter Freyther <zecke@selfish.org>
2013-01-10 09:23:33 +0100

    nitb: Add ctrl command to set the BTS site id

--- openbsc/src/osmo-nitb/ctrl_commands.c
+++ openbsc/src/osmo-nitb/ctrl_commands.c
@@ -142,6 +142,8 @@ CTRL_CMD_DEFINE(net_apply_config, "apply-configuration");
 
 /* BTS related commands below here */
 CTRL_CMD_VTY_STRING(bts_description, "description", struct gsm_bts, description);
+CTRL_CMD_DEFINE_RANGE(bts_unit_id, "unit-id", struct gsm_bts,
+		ip_access.site_id, 0, 65534);
 
 
 int bsc_ctrl_cmds_install(void)
@@ -167,6 +169,7 @@ int bsc_ctrl_cmds_install(void)
 	rc |= ctrl_cmd_install(CTRL_NODE_ROOT, &cmd_net_timer_t3141);
 
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_description);
+	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_unit_id);
 
 	return rc;
 }
