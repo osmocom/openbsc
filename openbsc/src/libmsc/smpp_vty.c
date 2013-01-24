b862cef60de06ee71ee26a1c132aca77d9d0a7a3
Harald Welte <laforge@gnumonks.org>
2013-01-24 09:54:04 +0100

    smpp_vty: Ensure to not print 'system-id' in openbsc.cfg if its empty
    ... as an empty system-id would render the openbsc.cfg unparseable on
    next openbsc start.

--- openbsc/src/libmsc/smpp_vty.c
+++ openbsc/src/libmsc/smpp_vty.c
@@ -119,7 +119,8 @@ static int config_write_smpp(struct vty *vty)
 
 	vty_out(vty, "smpp%s", VTY_NEWLINE);
 	vty_out(vty, " local-tcp-port %u%s", smsc->listen_port, VTY_NEWLINE);
-	vty_out(vty, " system-id %s%s", smsc->system_id, VTY_NEWLINE);
+	if (smsc->system_id)
+		vty_out(vty, " system-id %s%s", smsc->system_id, VTY_NEWLINE);
 	vty_out(vty, " policy %s%s",
 		smsc->accept_all ? "accept-all" : "closed", VTY_NEWLINE);
 
