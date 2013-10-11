ce6ab151bddbbcf06178c04600020f679e07bbba
Andreas Eversberg <jolly@eversberg.eu>
2013-10-11 13:01:38 +0200

    dyn PDCH: Add new_lchan argument to bsc_handover_start()
    This is useful if the caller already allocated a new lchan, which will be used
    to dynamically re-assign lchans.
    
    The old behavior is maintained by passing NULL.
    
    Change-Id: I2b7151f32f0c04c22f294eb5dd3c7d7dfddf35e7

--- openbsc/include/openbsc/handover.h
+++ openbsc/include/openbsc/handover.h
@@ -3,7 +3,8 @@
 
 struct gsm_subscriber_connection;
 
-int bsc_handover_start(struct gsm_lchan *old_lchan, struct gsm_bts *bts);
+int bsc_handover_start(struct gsm_lchan *old_lchan, struct gsm_lchan *new_lchan,
+		       struct gsm_bts *bts);
 
 /* clear any operation for this connection */
 void bsc_clear_handover(struct gsm_subscriber_connection *conn, int free_lchan);
