94f83e1787dfaf0014963783c8602b79cad9ae2e
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2014-07-19 19:02:46 +0200

    smpp: Fix small typo in the comment

--- openbsc/src/libmsc/smpp_openbsc.c
+++ openbsc/src/libmsc/smpp_openbsc.c
@@ -107,7 +107,7 @@ static int submit_to_sms(struct gsm_sms **psms, struct gsm_network *net,
 	t = find_tlv(submit->tlv, TLVID_message_payload);
 	if (t) {
 		if (submit->sm_length) {
-			/* ERROR: we cannot have botH! */
+			/* ERROR: we cannot have both! */
 			LOGP(DLSMS, LOGL_ERROR, "SMPP Cannot have payload in "
 				"TLV _and_ in the header\n");
 			return ESME_ROPTPARNOTALLWD;
