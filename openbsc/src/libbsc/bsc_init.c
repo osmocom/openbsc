c58a1e573b4eedc213f210b97f6a3d82ffdc056f
Michael McTernan <mike.mcternan@wavemobile.com>
2015-06-19 20:51:43 +0200

    Fix GSM900 ARFCN range check
    Allow ARFCN 0 to be used in GSM900 band.

--- openbsc/src/libbsc/bsc_init.c
+++ openbsc/src/libbsc/bsc_init.c
@@ -397,10 +397,10 @@ static int bootstrap_bts(struct gsm_bts *bts)
 		}
 		break;
 	case GSM_BAND_900:
-		if (bts->c0->arfcn < 1 ||
+		if (bts->c0->arfcn < 0 ||
 		   (bts->c0->arfcn > 124 && bts->c0->arfcn < 955) ||
 		    bts->c0->arfcn > 1023)  {
-			LOGP(DNM, LOGL_ERROR, "GSM900 channel must be between 1-124, 955-1023.\n");
+			LOGP(DNM, LOGL_ERROR, "GSM900 channel must be between 0-124, 955-1023.\n");
 			return -EINVAL;
 		}
 		break;
