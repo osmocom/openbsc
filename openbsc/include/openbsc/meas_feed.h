08c508f84a8d088f8454d0f21f83ecc22e30d72d
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2015-08-20 20:48:39 -0400

    msc: Add channel information to the meas_feed, bump version to v1.

--- openbsc/include/openbsc/meas_feed.h
+++ openbsc/include/openbsc/meas_feed.h
@@ -17,13 +17,25 @@ struct meas_feed_meas {
 	char name[31+1];
 	char scenario[31+1];
 	struct gsm_meas_rep mr;
+	/* The logical channel type, enum gsm_chan_t */
+	uint8_t lchan_type;
+	/* The physical channel type, enum gsm_phys_chan_config */
+	uint8_t pchan_type;
+	/* number of ths BTS in network */
+	uint8_t bts_nr;
+	/* number of this TRX in the BTS */
+	uint8_t trx_nr;
+	/* number of this timeslot at the TRX */
+	uint8_t ts_nr;
+	/* The logical subslot number in the TS */
+	uint8_t ss_nr;
 };
 
 enum meas_feed_msgtype {
 	MEAS_FEED_MEAS		= 0,
 };
 
-#define MEAS_FEED_VERSION	0
+#define MEAS_FEED_VERSION	1
 
 
 #endif
