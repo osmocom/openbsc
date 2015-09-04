79135acbb88c92bbe0214ada4feea53cb12a0ef4
Michael McTernan <mike.mcternan@wavemobile.com>
2015-09-04 15:57:47 +0200

    mgcp: Remove use of hardwired @mgw from endpoint IDs.
    Remove the host portion of the endpoint Id.  This requires less
    configuration and we are probably fine to trust that MGCP only
    received messages designated for it.

--- openbsc/src/libmgcp/mgcp_protocol.c
+++ openbsc/src/libmgcp/mgcp_protocol.c
@@ -431,7 +431,7 @@ static struct mgcp_endpoint *find_endpoint(struct mgcp_config *cfg, const char *
 		return find_e1_endpoint(cfg, mgcp);
 	} else {
 		gw = strtoul(mgcp, &endptr, 16);
-		if (gw > 0 && gw < cfg->trunk.number_endpoints && strcmp(endptr, "@mgw") == 0)
+		if (gw > 0 && gw < cfg->trunk.number_endpoints && endptr[0] == '@')
 			return &cfg->trunk.endpoints[gw];
 	}
 
