69ca8f01ec78ff33ec3cd5d4451e93c53b378f35
Harald Welte <laforge@gnumonks.org>
2015-10-29 10:35:24 +0100

    SGSN: Fix typo in VTY license statement.
    It mentioned AGPLv3+ and refers to the hyperlink of v3, but still
    stated "Version 2 or later".  That was a typo at the time.

--- openbsc/src/gprs/sgsn_main.c
+++ openbsc/src/gprs/sgsn_main.c
@@ -67,7 +67,7 @@ struct gprs_ns_inst *sgsn_nsi;
 static int daemonize = 0;
 const char *openbsc_copyright =
 	"Copyright (C) 2010 Harald Welte and On-Waves\r\n"
-	"License AGPLv3+: GNU AGPL version 2 or later <http://gnu.org/licenses/agpl-3.0.html>\r\n"
+	"License AGPLv3+: GNU AGPL version 3 or later <http://gnu.org/licenses/agpl-3.0.html>\r\n"
 	"This is free software: you are free to change and redistribute it.\r\n"
 	"There is NO WARRANTY, to the extent permitted by law.\r\n";
 
