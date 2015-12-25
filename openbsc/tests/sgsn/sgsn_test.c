49393e128e759993e7da74c076d7dd1c47705638
Harald Welte <laforge@gnumonks.org>
2015-12-25 19:14:57 +0100

    rename gsm0408_gprs_rcvmsg() to gsm0408_gprs_rcvmsg_gb()
    This is the entry point for GMM from Gb.  We will create a new one
    for Iu, so let's be explicit rather than implicit.
    
    Change-Id: I93c074bf99db041117c0dc03dc8255879845a875

--- openbsc/tests/sgsn/sgsn_test.c
+++ openbsc/tests/sgsn/sgsn_test.c
@@ -196,7 +196,7 @@ static void send_0408_message(struct gprs_llc_llme *llme, uint32_t tlli,
 	msg = create_msg(data, data_len);
 	msgb_tlli(msg) = tlli;
 	bssgp_create_cell_id(msgb_bcid(msg), bssgp_raid, 0);
-	gsm0408_gprs_rcvmsg(msg, llme);
+	gsm0408_gprs_rcvmsg_gb(msg, llme);
 	msgb_free(msg);
 }
 
