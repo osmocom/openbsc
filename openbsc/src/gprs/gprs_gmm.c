2b2429eb59af1edc9e8760a441c8c457418a459c
Harald Welte <laforge@gnumonks.org>
2015-12-25 22:31:36 +0100

    gprs_gmm.c: Perform LLME operations only if we have one
    In case the GMM message did not arrive over a Gb interface, there is no
    LLME (and thus the associated pointer is NULL).  Don't try to perform
    operations on a NULL LLME.
    
    Change-Id: If7f24161cd2826f8ee238d4bc1090adf555cea4e

--- openbsc/src/gprs/gprs_gmm.c
+++ openbsc/src/gprs/gprs_gmm.c
@@ -1208,10 +1208,12 @@ static int gsm48_rx_gmm_ra_upd_req(struct sgsn_mm_ctx *mmctx, struct msgb *msg,
 	}
 
 	if (!mmctx) {
-		/* send a XID reset to re-set all LLC sequence numbers
-		 * in the MS */
-		LOGMMCTXP(LOGL_NOTICE, mmctx, "LLC XID RESET\n");
-		gprs_llgmm_reset(llme);
+		if (llme) {
+			/* send a XID reset to re-set all LLC sequence numbers
+			 * in the MS */
+			LOGMMCTXP(LOGL_NOTICE, mmctx, "LLC XID RESET\n");
+			gprs_llgmm_reset(llme);
+		}
 		/* The MS has to perform GPRS attach */
 		/* Device is still IMSI attached for CS but initiate GPRS ATTACH,
 		 * see GSM 04.08, 4.7.5.1.4 and G.6 */
@@ -1314,7 +1316,7 @@ static int gsm0408_rcv_gmm(struct sgsn_mm_ctx *mmctx, struct msgb *msg,
 
 	/* MMCTX can be NULL when called */
 
-	if (!mmctx &&
+	if (llme && !mmctx &&
 	    gh->msg_type != GSM48_MT_GMM_ATTACH_REQ &&
 	    gh->msg_type != GSM48_MT_GMM_RA_UPD_REQ) {
 		LOGP(DMM, LOGL_NOTICE, "Cannot handle GMM for unknown MM CTX\n");
