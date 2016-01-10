2449c0ce58f01a78944073adebca70bb21639689
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-01-10 22:50:11 +0100

    tweak ..rcvmsg_iu()'s sai arg.
    It was a uint16_t*, but is passed as a uint16_t, and never used anyway, yet.

--- openbsc/include/openbsc/iu.h
+++ openbsc/include/openbsc/iu.h
@@ -4,6 +4,7 @@ struct msgb;
 struct gprs_ra_id;
 
 typedef int (* iu_recv_cb_t )(struct msgb *msg, struct gprs_ra_id *ra_id,
-			      uint16_t *sai);
+			      /* TODO is ra_id only used for gprs? ^ */
+			      uint16_t sai);
 
 int iu_tx(struct msgb *msg, uint8_t sapi);
