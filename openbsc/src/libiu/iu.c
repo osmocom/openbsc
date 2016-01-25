82724653e7e76bb2c3e1e03efb033d7b3704adb3
Daniel Willmann <dwillmann@sysmocom.de>
2016-01-25 16:53:20 +0100

    sgsn_iu: RABAssignment response is an Outcome, not a Successful one

--- openbsc/src/libiu/iu.c
+++ openbsc/src/libiu/iu.c
@@ -292,10 +292,6 @@ static void cn_ranap_handle_co(void *ctx, ranap_message *message)
 		break;
 	case RANAP_RANAP_PDU_PR_successfulOutcome:
 		switch (message->procedureCode) {
-		case RANAP_ProcedureCode_id_RAB_Assignment:
-			/* RAB Assignment Response */
-			rc = ranap_handle_co_rab_ass_resp(ctx, &message->msg.raB_AssignmentResponseIEs);
-			break;
 		case RANAP_ProcedureCode_id_SecurityModeControl:
 			/* Security Mode Complete */
 			rc = -1;
@@ -308,8 +304,14 @@ static void cn_ranap_handle_co(void *ctx, ranap_message *message)
 			rc = -1;
 			break;
 		}
-	case RANAP_RANAP_PDU_PR_unsuccessfulOutcome:
 	case RANAP_RANAP_PDU_PR_outcome:
+		switch (message->procedureCode) {
+		case RANAP_ProcedureCode_id_RAB_Assignment:
+			/* RAB Assignment Response */
+			rc = ranap_handle_co_rab_ass_resp(ctx, &message->msg.raB_AssignmentResponseIEs);
+			break;
+		}
+	case RANAP_RANAP_PDU_PR_unsuccessfulOutcome:
 	default:
 		rc = -1;
 		break;
