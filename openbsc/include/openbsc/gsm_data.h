d1646ade316ea187ff511df4da6936803f8a1ff9
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-03-16 15:53:49 +0100

    bts: Backport is_sysmobts_v2 from master
    Backported from c5e6daa8913dd02aa85ea05b8888e71bd0db0fc9 and
    needed for the restart handling

--- openbsc/include/openbsc/gsm_data.h
+++ openbsc/include/openbsc/gsm_data.h
@@ -357,6 +357,17 @@ static inline int is_ipaccess_bts(struct gsm_bts *bts)
 	return 0;
 }
 
+static inline int is_sysmobts_v2(struct gsm_bts *bts)
+{
+	switch (bts->type) {
+	case GSM_BTS_TYPE_OSMO_SYSMO:
+		return 1;
+	default:
+		break;
+	}
+	return 0;
+}
+
 static inline int is_siemens_bts(struct gsm_bts *bts)
 {
 	switch (bts->type) {
