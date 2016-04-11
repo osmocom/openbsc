54fc3a13183e4d5956c8c17b74caf7ef21febe0a
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-04-11 20:25:28 +0200

    remove subscr_conn_allocate_iu() from public header
    It is only used statically in iu.c

--- openbsc/include/openbsc/iu_cs.h
+++ openbsc/include/openbsc/iu_cs.h
@@ -2,9 +2,5 @@
 
 int gsm0408_rcvmsg_iucs(struct gsm_network *network, struct msgb *msg);
 
-struct gsm_subscriber_connection *subscr_conn_allocate_iu(struct gsm_network *network,
-							  uint8_t link_id,
-							  uint32_t conn_id);
-
 struct gsm_subscriber_connection *subscr_conn_lookup_iu(struct gsm_network *network,
 							struct ue_conn_ctx *ue);
