ac1f1436e9d380f632dd850fcd253d3480f0fc2d
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-05-10 13:30:17 +0200

    factor out & introduce struct gsm_encr, in common_cs.h
    Factor out encryption info from struct gsm_lchan as struct gsm_encr, placed in
    common_cs.h.
    
    Change-Id: I94015fb9dd511c37c1e3058a0963c780b3f700ac
    Future: this will be used by libmsc's subscriber connection, for osmo-cscn.

--- openbsc/include/openbsc/common_cs.h
+++ openbsc/include/openbsc/common_cs.h
@@ -7,6 +7,14 @@ struct gsm_network;
 
 typedef int (*mncc_recv_cb_t)(struct gsm_network *, struct msgb *);
 
+#define MAX_A5_KEY_LEN	(128/8)
+
+struct gsm_encr {
+	uint8_t alg_id;
+	uint8_t key_len;
+	uint8_t key[MAX_A5_KEY_LEN];
+};
+
 struct gsm_network *gsm_network_init(void *ctx,
 				     uint16_t country_code,
 				     uint16_t network_code,
