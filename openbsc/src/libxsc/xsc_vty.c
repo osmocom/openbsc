f0773e14f861d4e26985c380865438fa34c34617
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-05-11 18:48:39 +0200

    move to libxsc: global vty gsm_network pointer
    Move gsmnet_from_vty() and the bsc_gsmnet global to xsc_vty.c.
    
    Rename bsc_gsmnet to vty_global_gsm_network and make it static to xsc_vty.c, to
    clearly mark the global variable for VTY use only.
    
    Introduce xsc_vty_init() to set vty_global_gsm_network.
    
    Change-Id: I26c5c47de08f899b896813d09612d5cb2f8e42d6

--- openbsc/src/libxsc/xsc_vty.c
+++ openbsc/src/libxsc/xsc_vty.c
@@ -1,6 +1,7 @@
 /* Code used by both libbsc and libmsc (xsc means "BSC or MSC").
  *
  * (C) 2016 by sysmocom s.m.f.c. <info@sysmocom.de>
+ * (C) 2008-2010 by Harald Welte <laforge@gnumonks.org>
  * All Rights Reserved
  *
  * This program is free software; you can redistribute it and/or modify
@@ -17,3 +18,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
+#include <openbsc/gsm_data.h>
+
+static struct gsm_network *vty_global_gsm_network = NULL;
+
+/* initialize VTY elements used in both BSC and MSC */
+int xsc_vty_init(struct gsm_network *network)
+{
+	OSMO_ASSERT(vty_global_gsm_network == NULL);
+	vty_global_gsm_network = network;
+}
+
+struct gsm_network *gsmnet_from_vty(struct vty *v)
+{
+	/* It can't hurt to force callers to continue to pass the vty instance
+	 * to this function, in case we'd like to retrieve the global
+	 * gsm_network instance from the vty at some point in the future. But
+	 * until then, just return the global pointer, which should have been
+	 * initialized by xsc_vty_init().
+	 */
+	OSMO_ASSERT(vty_global_gsm_network);
+	return vty_global_gsm_network;
+}
