7c359eb4b45c382ff3ec5387cbd035a11c8041b4
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-09-16 01:44:52 +0200

    cosmetic fixes in libcommon/talloc_ctx.c
    Add copyright notice, remove obsolete include, remove unneeded line break.
    
    Change-Id: I4d06a0323aee5a003b06edd179fc61e1936acae5

--- openbsc/src/libcommon/talloc_ctx.c
+++ openbsc/src/libcommon/talloc_ctx.c
@@ -1,6 +1,25 @@
-#include <osmocom/core/talloc.h>
-#include <openbsc/gsm_data.h>
+/* OpenBSC allocation contexts initialization code */
+/* (C) 2011-2016 by sysmocom s.f.m.c. GmbH <info@sysmocom.de>
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
+#include <osmocom/core/talloc.h>
+
+extern void *tall_bsc_ctx;
 extern void *tall_msgb_ctx;
 extern void *tall_fle_ctx;
 extern void *tall_locop_ctx;
