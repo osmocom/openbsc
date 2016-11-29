osmocom/openbsc

commit 95db69b7702a13b70cb5e796b06232a6a041bbed

libbsc: add chreq type for CHREQ_T_PDCH_ONE_PHASE & CHREQ_T_PDCH_TWO_PHASE
When using a BSC located PCU the BSC must understand PDCH requests.

Change-Id: Ie7f4ed000cf1b40d269873cf0ddf5ff9f5bbc18a

openbsc/src/libbsc/gsm_04_08_utils.c
@@ -75,8 +75,12 @@ static const struct chreq chreq_type_neci1[] = {
 	{ 0x67, 0xff, CHREQ_T_LMU },
 	{ 0x60, 0xf9, CHREQ_T_RESERVED_SDCCH },
 	{ 0x61, 0xfb, CHREQ_T_RESERVED_SDCCH },
-	{ 0x63,	0xff, CHREQ_T_RESERVED_SDCCH },
-	{ 0x7f, 0xff, CHREQ_T_RESERVED_IGNORE },
+	{ 0x63, 0xff, CHREQ_T_RESERVED_SDCCH },
+	{ 0x70, 0xf8, CHREQ_T_PDCH_TWO_PHASE },
+	{ 0x78, 0xfc, CHREQ_T_PDCH_ONE_PHASE },
+	{ 0x79, 0xfa, CHREQ_T_PDCH_ONE_PHASE },
+	{ 0x7a, 0xf9, CHREQ_T_PDCH_ONE_PHASE },
+	{ 0x7f, 0xf, CHREQ_T_RESERVED_IGNORE },
 };
 
 /* If SYSTEM INFORMATION TYPE 4 NECI bit == 0 */
@@ -92,7 +96,11 @@ static const struct chreq chreq_type_neci0[] = {
 	{ 0x67, 0xff, CHREQ_T_LMU },
 	{ 0x60, 0xf9, CHREQ_T_RESERVED_SDCCH },
 	{ 0x61, 0xfb, CHREQ_T_RESERVED_SDCCH },
-	{ 0x63,	0xff, CHREQ_T_RESERVED_SDCCH },
+	{ 0x63, 0xff, CHREQ_T_RESERVED_SDCCH },
+	{ 0x70, 0xf8, CHREQ_T_PDCH_TWO_PHASE },
+	{ 0x78, 0xfc, CHREQ_T_PDCH_ONE_PHASE },
+	{ 0x79, 0xfa, CHREQ_T_PDCH_ONE_PHASE },
+	{ 0x7a, 0xf9, CHREQ_T_PDCH_ONE_PHASE },
 	{ 0x7f, 0xff, CHREQ_T_RESERVED_IGNORE },
 };
 
@@ -112,6 +120,8 @@ static const enum gsm_chan_t ctype_by_chreq[] = {
 	[CHREQ_T_PAG_R_TCH_FH]		= GSM_LCHAN_TCH_F,
 	[CHREQ_T_LMU]			= GSM_LCHAN_SDCCH,
 	[CHREQ_T_RESERVED_SDCCH]	= GSM_LCHAN_SDCCH,
+	[CHREQ_T_PDCH_ONE_PHASE]	= GSM_LCHAN_PDTCH,
+	[CHREQ_T_PDCH_TWO_PHASE]	= GSM_LCHAN_PDTCH,
 	[CHREQ_T_RESERVED_IGNORE]	= GSM_LCHAN_UNKNOWN,
 };
 
@@ -130,6 +140,8 @@ static const enum gsm_chreq_reason_t reason_by_chreq[] = {
 	[CHREQ_T_PAG_R_TCH_F]		= GSM_CHREQ_REASON_PAG,
 	[CHREQ_T_PAG_R_TCH_FH]		= GSM_CHREQ_REASON_PAG,
 	[CHREQ_T_LMU]			= GSM_CHREQ_REASON_OTHER,
+	[CHREQ_T_PDCH_ONE_PHASE]	= GSM_CHREQ_REASON_PBCH,
+	[CHREQ_T_PDCH_TWO_PHASE]	= GSM_CHREQ_REASON_PBCH,
 	[CHREQ_T_RESERVED_SDCCH]	= GSM_CHREQ_REASON_OTHER,
 	[CHREQ_T_RESERVED_IGNORE]	= GSM_CHREQ_REASON_OTHER,
 };

openbsc/include/openbsc/gsm_data.h
@@ -338,7 +338,7 @@ struct gsm_network {
 		enum rrlp_mode mode;
 	} rrlp;
 
-	enum gsm_chan_t ctype_by_chreq[16];
+	enum gsm_chan_t ctype_by_chreq[18];
 
 	/* Use a TCH for handling requests of type paging any */
 	int pag_any_tch;

commit cc9e9f47cfa5d2953e019ffdc72b169edb99e530

libbsc: fix wrong mask introduced by 95db69b7 in chreq
Change-Id: I06568afe2c4728d27dcce0c84cedd7914abf8264

openbsc/src/libbsc/gsm_04_08_utils.c
@@ -80,7 +80,7 @@ static const struct chreq chreq_type_neci1[] = {
 	{ 0x78, 0xfc, CHREQ_T_PDCH_ONE_PHASE },
 	{ 0x79, 0xfa, CHREQ_T_PDCH_ONE_PHASE },
 	{ 0x7a, 0xf9, CHREQ_T_PDCH_ONE_PHASE },
-	{ 0x7f, 0xf, CHREQ_T_RESERVED_IGNORE },
+	{ 0x7f, 0xff, CHREQ_T_RESERVED_IGNORE },
 };
 
 /* If SYSTEM INFORMATION TYPE 4 NECI bit == 0 */