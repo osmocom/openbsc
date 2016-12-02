05a5b1a750b50a4022f1036244b9595f7b2804c9
Alexander Couzens <lynxis@fe80.eu>
2016-12-02 05:21:45 +0100

    libbsc: add ericsson specific f1 trail to receive a confirmation
    Change-Id: I0fdb279ce178037f99a698b99391a1720ef45c30

--- openbsc/src/libbsc/abis_rsl.c
+++ openbsc/src/libbsc/abis_rsl.c
@@ -1073,6 +1073,11 @@ int rsl_imm_assign_cmd(struct gsm_bts *bts, uint8_t len, uint8_t *val)
 	}
 
 	msg->dst = bts->c0->rsl_link;
+	msgb_put_u8(msg, 0xf1);
+	msgb_put_u8(msg, random() & 0xff);
+	msgb_put_u8(msg, random() & 0xff);
+	msgb_put_u8(msg, random() & 0xff);
+	msgb_put_u8(msg, 0x0);
 
 	return abis_rsl_sendmsg(msg);
 }
