94bbc73bce304609124e9f617953a11634905d58
Philipp Maier <pmaier@sysmocom.de>
2016-12-08 17:34:12 +0100

    libbsc: add debug log message to S_L_INP_* callbacks
    Improve debug log output of input callbacks by adding a line containing
    the signal event name.
    
    Change-Id: Ifca46dd8b356d0de31cccbd79e406079d3a0d7d2

--- openbsc/src/libbsc/bsc_init.c
+++ openbsc/src/libbsc/bsc_init.c
@@ -320,6 +320,7 @@ static int inp_sig_cb(unsigned int subsys, unsigned int signal,
 
 	switch (signal) {
 	case S_L_INP_TEI_UP:
+		LOGP(DLMI, LOGL_DEBUG, "%s() signal: S_L_INP_TEI_UP\n", __func__);
 		if (isd->link_type == E1INP_SIGN_OML) {
 			/* TODO: this is required for the Nokia BTS, hopping is configured
 			   during OML, other MA is not set.  */
@@ -344,6 +345,7 @@ static int inp_sig_cb(unsigned int subsys, unsigned int signal,
 			bootstrap_rsl(trx);
 		break;
 	case S_L_INP_TEI_DN:
+		LOGP(DLMI, LOGL_DEBUG, "%s() signal: S_L_INP_TEI_DN\n", __func__);
 		LOGP(DLMI, LOGL_ERROR, "Lost some E1 TEI link: %d %p\n", isd->link_type, trx);
 
 		if (isd->link_type == E1INP_SIGN_OML)
