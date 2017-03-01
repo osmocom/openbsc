ac27a99021d404f9a15b952fbbc03789926b7ecc
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-03-01 15:06:16 +0100

    jenkins: add value_string termination check
    Depends: libosmocore change-id I2bc93ab4781487e7685cfb63091a489cd126b1a8
    Change-Id: I4183415cd1ead9d46ae3a556e94243325ef5a844

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -12,6 +12,8 @@ rm -rf "$inst"
 
 osmo-build-dep.sh libosmocore "" ac_cv_path_DOXYGEN=false
 
+"$deps"/libosmocore/contrib/verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
+
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
 
