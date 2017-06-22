osmocom/openbsc

commit a60bb3dd28ce9e3720f8ee1b262893f3e233e2e6

OML: fix potential OOB memory access
Use sizeof target BTS feature storage to make sure we always fit into
pre-allocated memory. Also use it for log check.

Change-Id: Ib107daa6e8b9bc397a10756071849f8ff82455d5
Fixes: CID 170581

openbsc/src/libbsc/abis_nm.c
@@ -490,13 +490,13 @@ static inline uint8_t *parse_attr_resp_info_manuf_id(struct gsm_bts *bts, uint8_
 			m_id_len = MAX_BTS_FEATURES/8;
 		}
 
-		if (m_id_len > _NUM_BTS_FEAT/8 + 1)
+		if (m_id_len > sizeof(bts->_features_data))
 			LOGP(DNM, LOGL_NOTICE, "BTS%u Get Attributes Response: reported unexpectedly long (%u bytes) "
 			     "feature vector - most likely it was compiled against newer BSC headers. "
 			     "Consider upgrading your BSC to later version.\n",
 			     bts->nr, m_id_len);
 
-		memcpy(bts->_features_data, TLVP_VAL(&tp, NM_ATT_MANUF_ID), m_id_len);
+		memcpy(bts->_features_data, TLVP_VAL(&tp, NM_ATT_MANUF_ID), sizeof(bts->_features_data));
 		adjust = m_id_len + 3; /* adjust for parsed TL16V struct */
 
 		for (i = 0; i < _NUM_BTS_FEAT; i++)

commit 5a44d25ad343d6426295480f6a0a5d6dc21350cf

Fix BTS features length check
While fixing potentially incorrect memory access, the check for maximum
number of supported BTS features was incorrectly adjusted instead of
feature vectore length check next to it. Fix this by adjusting checks
properly and adding comments to avoid future confusion.

The error was introduced in a60bb3dd28ce9e3720f8ee1b262893f3e233e2e6.

Change-Id: I06d2498d730624d5da535f6add6fa98d004714ae

openbsc/src/libbsc/abis_nm.c
@@ -484,13 +484,13 @@ static inline uint8_t *parse_attr_resp_info_manuf_id(struct gsm_bts *bts, uint8_
 	if (TLVP_PRES_LEN(&tp, NM_ATT_MANUF_ID, 2)) {
 		m_id_len = TLVP_LEN(&tp, NM_ATT_MANUF_ID);
 
-		if (m_id_len > MAX_BTS_FEATURES/8 + 1) {
+		/* log potential BTS feature vector overflow */
+		if (m_id_len > sizeof(bts->_features_data))
 			LOGP(DNM, LOGL_NOTICE, "BTS%u Get Attributes Response: feature vector is truncated to %u bytes\n",
 			     bts->nr, MAX_BTS_FEATURES/8);
-			m_id_len = MAX_BTS_FEATURES/8;
-		}
 
-		if (m_id_len > sizeof(bts->_features_data))
+		/* check that max. expected BTS attribute is above given feature vector length */
+		if (m_id_len > OSMO_BYTES_FOR_BITS(_NUM_BTS_FEAT))
 			LOGP(DNM, LOGL_NOTICE, "BTS%u Get Attributes Response: reported unexpectedly long (%u bytes) "
 			     "feature vector - most likely it was compiled against newer BSC headers. "
 			     "Consider upgrading your BSC to later version.\n",