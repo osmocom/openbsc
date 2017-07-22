73d7febc0e57010bddc7d1044e493b0f71428aca
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-07-22 18:20:48 +0200

    msc_vlr_tests: add missing wrap for iu_tx_release()
    It seems merely luck that the tests worked so far without this wrap. Adjust
    test logs now indicating where we actually send an Iu Release -- and leaving
    gaping holes where we don't. To be fixed in subsequent commit.
    
    Change-Id: Icf8b0b4acd8774c72cff6fdba0ff549308fc506f

--- openbsc/tests/msc_vlr/msc_vlr_tests.c
+++ openbsc/tests/msc_vlr/msc_vlr_tests.c
@@ -475,6 +475,13 @@ int __wrap_iu_tx(struct msgb *msg, uint8_t sapi)
 	return _validate_dtap(msg, RAN_UTRAN_IU);
 }
 
+/* override, requires '-Wl,--wrap=iu_tx_release' */
+int __real_iu_tx_release(struct ue_conn_ctx *ctx, const struct RANAP_Cause *cause);
+int __wrap_iu_tx_release(struct ue_conn_ctx *ctx, const struct RANAP_Cause *cause)
+{
+	btw("Iu Release --%s--> MS", ran_type_name(RAN_UTRAN_IU));
+}
+
 /* override, requires '-Wl,--wrap=a_iface_tx_dtap' */
 int __real_a_iface_tx_dtap(struct msgb *msg);
 int __wrap_a_iface_tx_dtap(struct msgb *msg)
