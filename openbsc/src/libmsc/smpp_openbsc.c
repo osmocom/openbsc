18ca1ce2ea9f0c353e6886a9d05c61238bba9dc6
Pablo Neira Ayuso <pablo@gnumonks.org>
2017-08-07 14:01:01 +0100

    libmsc: do not leak pending SMPP command object on error path
    Make sure the SMPP command object is released on errors.
    
    Change-Id: I474584425d23fb379a9d71b33e29ac0e24f01e61

--- openbsc/src/libmsc/smpp_openbsc.c
+++ openbsc/src/libmsc/smpp_openbsc.c
@@ -517,7 +517,7 @@ void smpp_cmd_ack(struct osmo_smpp_cmd *cmd)
 	conn = connection_for_subscr(cmd->subscr);
 	if (!conn) {
 		LOGP(DSMPP, LOGL_ERROR, "No connection to subscriber anymore\n");
-		return;
+		goto out;
 	}
 
 	trans = trans_find_by_id(conn, GSM48_PDISC_SMS,
@@ -525,10 +525,11 @@ void smpp_cmd_ack(struct osmo_smpp_cmd *cmd)
 	if (!trans) {
 		LOGP(DSMPP, LOGL_ERROR, "GSM transaction %u is gone\n",
 		     cmd->sms->gsm411.transaction_id);
-		return;
+		goto out;
 	}
 
 	gsm411_send_rp_ack(trans, cmd->sms->gsm411.msg_ref);
+out:
 	smpp_cmd_free(cmd);
 }
 
@@ -541,7 +542,7 @@ void smpp_cmd_err(struct osmo_smpp_cmd *cmd, uint32_t status)
 	conn = connection_for_subscr(cmd->subscr);
 	if (!conn) {
 		LOGP(DSMPP, LOGL_ERROR, "No connection to subscriber anymore\n");
-		return;
+		goto out;
 	}
 
 	trans = trans_find_by_id(conn, GSM48_PDISC_SMS,
@@ -549,14 +550,14 @@ void smpp_cmd_err(struct osmo_smpp_cmd *cmd, uint32_t status)
 	if (!trans) {
 		LOGP(DSMPP, LOGL_ERROR, "GSM transaction %u is gone\n",
 		     cmd->sms->gsm411.transaction_id);
-		return;
+		goto out;
 	}
 
 	if (smpp_to_gsm411_err(status, &gsm411_cause) < 0)
 		gsm411_cause = GSM411_RP_CAUSE_MO_NET_OUT_OF_ORDER;
 
 	gsm411_send_rp_error(trans, cmd->sms->gsm411.msg_ref, gsm411_cause);
-
+out:
 	smpp_cmd_free(cmd);
 }
 
