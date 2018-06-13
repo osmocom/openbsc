osmocom/openbsc

commit d05d05b2773a1dc96a51104034942d504f2b1166

chan_alloc: Print bts nr on chan alloc failure
Change-Id: I51bb656b5fef3247edc63477f391c954c4b28f56

openbsc/src/libbsc/chan_alloc.c
@@ -362,8 +362,8 @@ struct gsm_lchan *lchan_alloc(struct gsm_bts *bts, enum gsm_chan_t type,
 	} else {
 		struct challoc_signal_data sig;
 
-		LOGP(DRLL, LOGL_ERROR, "Failed to allocate %s channel\n",
-		     gsm_lchant_name(type));
+		LOGP(DRLL, LOGL_ERROR, "(bts=%d) Failed to allocate %s channel\n",
+		     gsm_ts_and_pchan_name(bts->nr), gsm_lchant_name(type));
 
 		sig.bts = bts;
 		sig.type = type;

commit 28c4a62a7c1069ec257368230f62cf69e0da95a8

chan_alloc: Fix crash when failing to allocate channel
Fix a really silly bug I introduced recently (same commit in
osmo-bsc.git doesn't contain the issue).

Fixes: d05d05b2773a1dc96a51104034942d504f2b1166
Related: SYS#4254
Change-Id: I7bac2ce001d4a6dcea2a896af30edf84942b68de

openbsc/src/libbsc/chan_alloc.c
@@ -363,7 +363,7 @@ struct gsm_lchan *lchan_alloc(struct gsm_bts *bts, enum gsm_chan_t type,
 		struct challoc_signal_data sig;
 
 		LOGP(DRLL, LOGL_ERROR, "(bts=%d) Failed to allocate %s channel\n",
-		     gsm_ts_and_pchan_name(bts->nr), gsm_lchant_name(type));
+		     bts->nr, gsm_lchant_name(type));
 
 		sig.bts = bts;
 		sig.type = type;