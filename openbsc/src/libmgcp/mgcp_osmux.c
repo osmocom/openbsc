osmocom/openbsc

commit 65f422ad5878165be0a1eb05605aa3099536f0c8

mgcp: Fix osmux_cid_bitmap static array size calculation
Right now it's not a big issue since OSMUX_CID_MAX is 255, so 255+1 is
256 which fits array boundaries correctly (multiple of 8). However, if
for example OSMUC_CID_MAX was modified to be 12, 12+1/8 = 1, so we'd
have an undesired memory access when accessing last 4 CIDs.

Change-Id: I5908ee44404686e68d61f255b7014af39c8f5703

openbsc/src/libmgcp/mgcp_osmux.c
@@ -545,7 +545,7 @@ int osmux_send_dummy(struct mgcp_endpoint *endp)
 }
 
 /* bsc-nat allocates/releases the Osmux circuit ID */
-static uint8_t osmux_cid_bitmap[(OSMUX_CID_MAX + 1) / 8];
+static uint8_t osmux_cid_bitmap[(OSMUX_CID_MAX + 7) / 8];
 
 int osmux_used_cid(void)
 {

commit 6799c38eafa4c8138a0bf6e9f0d3390ec412c5df

mgcp: Fix (again) osmux_cid_bitmap static array size calculation
A previous commit merged today fixed array size boundary (multiple of 8
bits), but removed a required +1 which should be kept on top, because
OSMUX_CID_MAX specified the maximum number used by a CID, that is
(0,OSMUX_CID_MAX), and as a result we require OSMUX_CID_MAX+1 slots.

Fixes: 65f422ad5878165be0a1eb05605aa3099536f0c8
Change-Id: I182c9c1a6dd28a4c5c0d8107d53852cf47541592

openbsc/src/libmgcp/mgcp_osmux.c
@@ -544,8 +544,8 @@ int osmux_send_dummy(struct mgcp_endpoint *endp)
 			     htons(endp->cfg->osmux_port), buf, sizeof(buf));
 }
 
-/* bsc-nat allocates/releases the Osmux circuit ID */
-static uint8_t osmux_cid_bitmap[(OSMUX_CID_MAX + 7) / 8];
+/* bsc-nat allocates/releases the Osmux circuit ID. +7 to round up to 8 bit boundary. */
+static uint8_t osmux_cid_bitmap[(OSMUX_CID_MAX + 1 + 7) / 8];
 
 int osmux_used_cid(void)
 {