6887ffff18d4fbc5ed08593fe8d70573745e3811
Daniel Willmann <dwillmann@sysmocom.de>
2019-06-19 13:07:37 +0200

    manuals: Add script to regenerate counter/vty documentation
    Related: OS#1700
    Change-Id: I3e6e6603300d2d7f6221720fa7ce35b3a6aa0b7a

---
+++ openbsc/doc/regen_doc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+if [ -z "$DOCKER_PLAYGROUND" ]; then
+	echo "You need to set DOCKER_PLAYGROUND"
+	exit 1
+fi
+
+SCRIPT=$(realpath "$0")
+MANUAL_DIR=$(dirname "$SCRIPT")/../../manuals
+
+COMMIT=${COMMIT:-$(git log -1 --format=format:%H)}
+
+cd "$DOCKER_PLAYGROUND/scripts" || exit 1
+
+OSMO_NITB_BRANCH=$COMMIT ./regen_doc.sh osmo-nitb 4242 \
+	"$MANUAL_DIR/OsmoNITB/chapters/counters_generated.adoc" \
+	"$MANUAL_DIR/OsmoNITB/vty/nitb_vty_reference.xml"
+
+#OSMO_BSCNAT_BRANCH=$COMMIT ./regen_doc.sh osmo-bscnat 4244 \
+#	"$MANUAL_DIR/OsmoBSCNAT/chapters/counters_generated.adoc" \
+#	"$MANUAL_DIR/OsmoBSCNAT/vty/nat_vty_reference.xml"
